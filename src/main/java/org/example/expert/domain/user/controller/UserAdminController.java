8dd29486c9f81db28b8f31d483b475bf7f851b40
sinwoo-kim <shinwoo5676@gmail.com>
2025-01-15 19:13:23 +0900

    Fix: 레벨 1-3 AOP의 이해

--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.service.UserAdminService;
 import org.springframework.web.bind.annotation.PatchMapping;
